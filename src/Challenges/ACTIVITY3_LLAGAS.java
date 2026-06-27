a8c1d62b11fad508e212629ba406b94ed425ec79
Aldrennnnnnnnnnnnnn <aldrenmark2@gmail.com>
2026-06-27 18:58:17 +0800

    init commit

--- src/Challenges/ACTIVITY3_LLAGAS.java
+++ src/Challenges/ACTIVITY3_LLAGAS.java
@@ -1,43 +1,43 @@
 package Challenges;
 
-import java.util.Scanner;
+import java.util.Scanner;                       //Utility for scanner
 
 public class ACTIVITY3_LLAGAS {
     public static void main(String[] args){
 
         Scanner sc = new Scanner(System.in);
 
-        int snackPrice;
-        int snackChoice;
+        int snackPrice; //Stores the price of the selected snack
+        int snackChoice; //Stores the user's menu choice
 
-        System.out.println("\n------Snack Menu------");
+        System.out.println("\n------Snack Menu------"); //Display the choices
         System.out.println("1. Burger - ₱50");
         System.out.println("2. Fries - ₱40");
         System.out.println("3. Soda - ₱20");
         System.out.println("4. Ice cream - ₱35");
 
-        System.out.print("\nEnter your choice: ");
+        System.out.print("\nEnter your choice: "); //Accepting user input
         snackChoice = sc.nextInt();
 
-        switch(snackChoice){
-            case 1:
+        switch(snackChoice){                                //Use of switch case statement
+            case 1:                                         //if user choose 1, it prints the burger and it's price.
                 System.out.println("\nYou chose: Burger");
                 System.out.println("Price: ₱50");
-                break;
+                break;                                      //break
             case 2:
-                System.out.println("\nYou chose: Fries");
+                System.out.println("\nYou chose: Fries");   //if user choose 2 it prints the Fries and it's price.
                 System.out.println("Price: ₱40");
                 break;
             case 3:
-                System.out.println("\nYou chose: Soda");
+                System.out.println("\nYou chose: Soda");    //if user choose 3 it prints the Soda and it's price.
                 System.out.println("Price: ₱20");
                 break;
             case 4:
-                System.out.println("\nYou chose: Ice cream");
+                System.out.println("\nYou chose: Ice cream");   //if user choose 4 it prints the Ice cream and it's price.
                 System.out.println("Price: ₱35");
                 break;
             default:
-                System.out.println("\nInvalid choice");
+                System.out.println("\nInvalid choice");     //Displays an error message if the user enters an invalid menu option.
         }
     }
 }
--- src/Challenges/ACTIVITY4_LLAGAS.java
+++ src/Challenges/ACTIVITY4_LLAGAS.java
@@ -1,7 +1,7 @@
 package Challenges;
 
-import java.util.Scanner;
-import java.util.Random;
+import java.util.Scanner; //Utility for scanner or user input
+import java.util.Random;  //Utility to generate random number
 
 public class ACTIVITY4_LLAGAS {
     public static void main(String[] args){
@@ -9,28 +9,28 @@ public class ACTIVITY4_LLAGAS {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
-        int guess = 0;
-        int attempt = 0;
-        int ranDom = random.nextInt(1, 11);
+        int guess = 0; // Stores the user guess
+        int attempt = 0;   // Stores the user attempt number
+        int ranDom = random.nextInt(1, 11); // Generates a random number from 1 to 10
 
 
-        System.out.println("------ Guess the number Game ------");
+        System.out.println("------ Guess the number Game ------");  //Display the game name
 
-        while(guess != ranDom){
-            System.out.print("Enter your guess from 1-10: ");
+        while(guess != ranDom){ // Continues to loop until the user guess the correct number
+            System.out.print("Enter your guess from 1-10: "); //Accepts user input
                 guess = scanner.nextInt();
 
-                attempt++;
+                attempt++; // Increment the number of attempt
 
-                if (guess > ranDom){
+                if (guess > ranDom){                // Informs the user that the guess is higher than the target number
                     System.out.println("Too high");
-                } else if(guess < ranDom){
+                } else if(guess < ranDom){          // Informs the user that the guess is lower than the target number
                     System.out.println("Too low");
                 }else {
-                    System.out.println("You've Won");
+                    System.out.println("You've Won");// Informs the user when the correct number is guessed
                 }
         }
-        System.out.println("Number of attempt/s: " + attempt);
+        System.out.println("Number of attempt/s: " + attempt); //Display the number of attempt
         scanner.close();
     }
 }
